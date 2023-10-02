ef3fdc51c09934574952e6e24f1e760d66c6546b
Polina Poznyak <polinappoznyak@gmail.com>
2023-10-02 23:53:16 +0300

    Add getDetailedPokemon in PokemonService

--- PokemonApplication/API/PokemonService.swift
+++ PokemonApplication/API/PokemonService.swift
@@ -18,4 +18,12 @@ struct PokemonService {
             completion(.failure(error))
         })
     }
+    
+    func getDetailedPokemon(id: Int, _ completion:@escaping (DetailPokemon) -> ()) {
+        Bundle.main.fetchData(url: "https://pokeapi.co/api/v2/pokemon/\(id)/", model: DetailPokemon.self) { data in
+            completion(data)
+        } failure: { error in
+            print(error)
+        }
+    }
 }
