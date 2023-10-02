72d550900bea1b7c01860421ff40697986744a39
Polina Poznyak <polinappoznyak@gmail.com>
2023-10-02 23:41:10 +0300

    Add TableView extensions and create showPokemon func in Presenter

--- PokemonApplication/Presenter/PokemonPresenter.swift
+++ PokemonApplication/Presenter/PokemonPresenter.swift
@@ -8,14 +8,40 @@
 import Foundation
 
 protocol PokemonPresenterProtocol {
-    
+    func showPokemon(offset: Int?, _ completion: @escaping ([Pokemon], String?) -> Void)
 }
 
 class PokemonPresenter: PokemonPresenterProtocol {
+    
+    private var currentPage: Int = -1
+    private var isFetchingData: Bool = false
     
     let interactor: PokemonInteractorProtocol
     
     init(interactor: PokemonInteractorProtocol) {
         self.interactor = interactor
+    }
+    
+    func showPokemon(offset: Int?, _ completion: @escaping ([Pokemon], String?) -> Void) {
+        var nextPageOffset = offset ?? -1
+        if isFetchingData {
+            return
+        }
+        isFetchingData = true
+        
+        currentPage += 1
+        nextPageOffset = currentPage * 20
+        
+        interactor.getPokemons(offset: nextPageOffset) { result in
+            switch result {
+            case .success(let (pokemons, nextPageUrl)):
+                self.isFetchingData = false
+                completion(pokemons, nextPageOffset >= 0 ? "https://pokeapi.co/api/v2/pokemon?offset=\(nextPageOffset)&limit=20" : nil)
+            case .failure(let error):
+                self.isFetchingData = false
+                print("Error fetching pokemons: \(error)")
+                completion([], nil)
+            }
+        }
     }
 }
--- PokemonApplication/View/ViewController.swift
+++ PokemonApplication/View/ViewController.swift
@@ -10,13 +10,65 @@ import UIKit
 class ViewController: UIViewController {
 
     // MARK: - IBOutlets
+    
     @IBOutlet weak var pokemonTable: UITableView!
+    
+    // MARK: - Properties
+    
+    var presenter: PokemonPresenterProtocol!
+    var viewModels: [Pokemon] = []
+    var nextPageUrl: String?
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        pokemonTable.delegate = self
+        pokemonTable.dataSource = self
+        
+        presenter = PokemonPresenter(interactor: PokemonInteractor(pokemonService: PokemonService()))
+            
+        presenter.showPokemon(offset: nil) { (viewModels, nextPageUrl) in
+            DispatchQueue.main.async {
+                self.viewModels = viewModels
+                self.nextPageUrl = nextPageUrl
+                self.pokemonTable.reloadData()
+            }
+        }
     }
-
-
 }
 
+// MARK: - Extensions
+
+extension ViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
+        if indexPath.row == viewModels.count - 1, let nextPageUrl = nextPageUrl {
+            presenter.showPokemon(offset: nil) { (newViewModels, newNextPageUrl) in
+                DispatchQueue.main.async {
+                    self.viewModels.append(contentsOf: newViewModels)
+                    self.nextPageUrl = newNextPageUrl
+                    self.pokemonTable.reloadData()
+                }
+            }
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 120
+    }
+}
+
+
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return viewModels.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "customCell") as! CustomTableViewCell
+        let viewModel = viewModels[indexPath.row]
+        
+        cell.nameLbl.text = viewModel.name
+        
+        return cell
+    }
+}
