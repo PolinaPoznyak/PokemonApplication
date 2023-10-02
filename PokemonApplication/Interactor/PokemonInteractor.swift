7d8135575352c35948ba4a9f1fdd500b02a99aa1
Polina Poznyak <polinappoznyak@gmail.com>
2023-10-02 23:15:25 +0300

    Add PokemonInteractor

---
+++ PokemonApplication/Interactor/PokemonInteractor.swift
@@ -0,0 +1,33 @@
+//
+//  PokemonInteractor.swift
+//  PokemonApplication
+//
+//  Created by Polina Poznyak on 2.10.23.
+//
+
+import Foundation
+
+protocol PokemonInteractorProtocol {
+    func getPokemons(offset: Int?, completion: @escaping (Result<([Pokemon], String?), Error>) -> Void)
+}
+
+class PokemonInteractor: PokemonInteractorProtocol {
+    let pokemonService: PokemonService
+    
+    init(pokemonService: PokemonService) {
+        self.pokemonService = pokemonService
+    }
+    
+    func getPokemons(offset: Int?, completion: @escaping (Result<([Pokemon], String?), Error>) -> Void) {
+        var apiUrl = "https://pokeapi.co/api/v2/pokemon?limit=20"
+        if let offset = offset {
+            apiUrl += "&offset=\(offset)"
+        }
+        
+        Bundle.main.fetchData(url: apiUrl, model: PokemonListResponse.self) { response in
+            completion(.success((response.results ?? [], response.next)))
+        } failure: { error in
+            completion(.failure(error))
+        }
+    }
+}
