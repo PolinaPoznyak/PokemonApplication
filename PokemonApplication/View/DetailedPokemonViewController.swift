1e79cdb28f8186caf86e8602be2443f479a81a34
Polina Poznyak <polinappoznyak@gmail.com>
2023-10-03 20:45:32 +0300

    Add simple Pokemon details page

---
+++ PokemonApplication/View/DetailedPokemonViewController.swift
@@ -0,0 +1,39 @@
+//
+//  DetailedPokemonViewController.swift
+//  PokemonApplication
+//
+//  Created by Polina Poznyak on 3.10.23.
+//
+
+import UIKit
+
+class DetailedPokemonViewController: UIViewController {
+
+    // MARK: - IBOutlets
+    
+    @IBOutlet weak var pokemonImg: UIImageView!
+    @IBOutlet weak var pokemonName: UILabel!
+    @IBOutlet weak var pokemonType: UILabel!
+    @IBOutlet weak var pokemonWeight: UILabel!
+    @IBOutlet weak var pokemonHeight: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
+// MARK: - Extensions
+
+extension UIImageView {
+    func loadFrom(URLAddress: String) {
+        guard let url = URL(string: URLAddress) else { return }
+        
+        DispatchQueue.main.async { [weak self] in
+            if let imageData = try? Data(contentsOf: url) {
+                if let loadedImage = UIImage(data: imageData) {
+                    self?.image = loadedImage
+                }
+            }
+        }
+    }
+}
